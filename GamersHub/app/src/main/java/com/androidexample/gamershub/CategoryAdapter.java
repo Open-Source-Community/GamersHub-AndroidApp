cb1820f0af433b02dac005311197fc5fb9221574
anieo <ali.maher.1460@gmail.com>
2018-04-07 18:12:59 +0200

    add icons and text to the TabLayout

--- GamersHub/app/src/main/java/com/androidexample/gamershub/CategoryAdapter.java
+++ GamersHub/app/src/main/java/com/androidexample/gamershub/CategoryAdapter.java
@@ -1,53 +1,58 @@
 package com.androidexample.gamershub;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.net.ConnectException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ali on 4/4/18.
  */
 
 public class CategoryAdapter extends FragmentPagerAdapter {
-
+    private ArrayList<Fragment> pageFragments=new ArrayList<>();
+    private List<Integer> navIcons= new ArrayList<>();
+    private List<Integer> navStrings= new ArrayList<>();
+    private Context contxt;
     public CategoryAdapter(Context Context, android.support.v4.app.FragmentManager fm){
         super(fm);
+        contxt=Context;
+    }
+
+    public void addPage(Fragment page,int icon,int string)
+    {
+        pageFragments.add(page);
+        navIcons.add(icon);
+        navStrings.add(string);
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return pageFragments.size();
     }
 
     @Override
     public Fragment getItem(int position) {
-        switch (position){
-            case  0:
-                return new GroupFragment();
-            case  1:
-                return new HomeFragment();
-            case  2:
-                return new SettingsFragment();
-            default:
-                return null;
-
-        }
+        return pageFragments.get(position);
     }
+
+
+    public int getImage(int position)
+    {
+        return navIcons.get(position);
+    }
+
+
 
     @Override
     public CharSequence getPageTitle(int position) {
-        switch (position){
-            case  0:
-                return "Group";
-            case  1:
-                return "Home";
-            case  2:
-                return "Settings";
-            default:
-                return null;
-
-        }
+        return contxt.getString(navStrings.get(position));
     }
 }
--- GamersHub/app/src/main/java/com/androidexample/gamershub/MainActivity.java
+++ GamersHub/app/src/main/java/com/androidexample/gamershub/MainActivity.java
@@ -4,27 +4,65 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private CategoryAdapter  adapter = new CategoryAdapter(this,getSupportFragmentManager());
 
+    private void setupViewAdapter(ViewPager viewpager)
+    {
+        adapter.addPage(new HomeFragment(),R.drawable.ic_home_white_24dp,R.string.nav_home);
+        adapter.addPage(new ChatFragment(),R.drawable.ic_chat_white_24dp,R.string.nav_chat);
+        adapter.addPage(new GroupFragment(),R.drawable.ic_group_white_24dp,R.string.nav_group);
+        adapter.addPage(new ListFragment(),R.drawable.ic_list_white_24dp,R.string.nav_list);
+        viewpager.setAdapter(adapter);
+        viewpager.setOffscreenPageLimit(adapter.getCount());
+    }
+    private void setupTabsContent(TabLayout taber)
+    {
+        for (int i = 0; i < adapter.getCount(); i++) {
+            LinearLayout tab = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.tab_layout, null);
+
+            // get child TextView and ImageView from this layout for the icon and label
+            TextView tab_label = (TextView) tab.findViewById(R.id.tab_text);
+            ImageView tab_icon = (ImageView) tab.findViewById(R.id.tab_icon);
+
+            // set the label text by getting the actual string value by its id
+            // by getting the actual resource value `getResources().getString(string_id)`
+            tab_label.setText(adapter.getPageTitle(i));
+
+            // set the home to be active at first
+            /*if (i == 0) {
+                tab_label.setTextColor(getResources().getColor(R.color.efent_color));
+                tab_icon.setImageResource(navIconsActive[i]);
+            } else {
+                tab_icon.setImageResource(navIcons[i]);
+            }*/
+            tab_icon.setImageResource(adapter.getImage(i));
+
+
+
+            // finally publish this custom view to navigation tab
+            taber.getTabAt(i).setCustomView(tab);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        CategoryAdapter adapter = new CategoryAdapter(this,getSupportFragmentManager());
-
         ViewPager pager =(ViewPager)findViewById(R.id.viewpager);
-
-        pager.setAdapter(adapter);
-        pager.setOffscreenPageLimit(adapter.getCount());
-
 
         TabLayout taber = (TabLayout) findViewById(R.id.tab);
 
+        setupViewAdapter(pager);
         taber.setupWithViewPager(pager);
+        setupTabsContent(taber);
 
     }
 }
